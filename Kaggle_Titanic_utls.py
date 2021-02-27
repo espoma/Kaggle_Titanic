127ff5879c56461b9d4fdbd512e9d975a72d2cd7
espoma <espoma@protonmail.ch>
2021-02-27 18:49:13 +0100

    fix

--- Kaggle_Titanic_utls.py
+++ Kaggle_Titanic_utls.py
@@ -16,14 +16,19 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
+#from sklearn.metrics import accuracy, make_scorer
 
 
 
 
-def base_pip(model, data, labels, imputer_s='median', option='train_test', param_grid={}):
-    # Pipeline + model
-    # Returns (score on train, score on test)
-    numeric_features = ['Age', 'SibSp', 'Fare', 'Parch']
+def base_pip(model, data, labels, imputer_s='median', option='train_test', 
+    param_grid=None, cv=3, numeric_features = ['Age', 'SibSp', 'Fare', 'Parch'], 
+    categorical_features = ['Embarked', 'Sex', 'Pclass', 'Title']):
+    # Pipeline + model or pipeline + grid search CV
+    # Returns (score on train, score on test) or (best_params, best_score, best_estimator)
+    
+
+    #numeric_features = ['Age', 'SibSp', 'Fare', 'Parch']
     
     # Mutual bit
     if (imputer_s == 'median'):
@@ -39,7 +44,7 @@ def base_pip(model, data, labels, imputer_s='median', option='train_test', param
         ('imputer', imputer),
         ('scaler', StandardScaler())])
 
-    categorical_features = ['Embarked', 'Sex', 'Pclass', 'Title']
+    #categorical_features = ['Embarked', 'Sex', 'Pclass', 'Title']
     categorical_transformer = OneHotEncoder(handle_unknown='ignore')
 
     preprocessor = ColumnTransformer(
@@ -50,16 +55,16 @@ def base_pip(model, data, labels, imputer_s='median', option='train_test', param
 
     pipeline = Pipeline(steps=[('preprocessor', preprocessor)])
 
-    
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.15,
                                                                     random_state=0, stratify=labels)
 
     X_train_tr = pipeline.fit_transform(X_train)
     X_test_tr = pipeline.transform(X_test)
+
+    print(X_train_tr.shape)
     
     clf = model
     
-
 
 
     # Baseline
@@ -77,14 +82,20 @@ def base_pip(model, data, labels, imputer_s='median', option='train_test', param
     # Grid Search
     elif (option == 'grid_search'):
         
-        gscv = GridSearchCV(clf, cv=3, n_jobs=-3, param_grid=param_grid, scoring='accuracy')
+        gscv = GridSearchCV(clf, cv=cv, n_jobs=-3, param_grid=param_grid, scoring='accuracy', verbose=1)
         gscv.fit(X_train_tr, y_train)
         best_params = gscv.best_params_
         best_score = gscv.best_score_
+        best_estimator = gscv.best_estimator_
+        #cv_results = gscv.cv_results_
 
-        return best_params, best_score
+        return best_params, best_score, best_estimator
 
 
+
+    elif (option == 'prediction'):
+
+        -----------
 
 
     else:
