9b8c974e3495a276d2786cec5b2c07d28145fb6e
espoma <espoma@protonmail.ch>
2021-03-06 11:06:03 +0100

    trials

--- Kaggle_Titanic_utls.py
+++ Kaggle_Titanic_utls.py
@@ -23,56 +23,96 @@ from sklearn.model_selection import cross_val_score
 
 
 
-
-def base_pip(model, data, labels, imputer_s=None, option='pipeline', 
-    pipeline=None, numeric_features = ['Age', 'SibSp', 'Fare', 'Parch'],
-    categorical_features = ['Embarked', 'Sex', 'Pclass', 'Title']):
+def base_pip(model, data, labels, imputer_s='median', option='train_test', 
+    param_grid=None, cv=3, numeric_features = ['Age', 'SibSp', 'Fare', 'Parch'],
+    categorical_features = ['Embarked', 'Sex', 'Pclass', 'Title'], X_unknown=None):
     # Pipeline + model or pipeline + grid search CV
     # Returns (score on train, score on test) or (best_params, best_score, best_estimator)
     
 
+    #numeric_features = ['Age', 'SibSp', 'Fare', 'Parch']
     
-    if (option == 'add_pipeline'):
-    
-	    if (imputer_s == 'median'):
-	            imputer = SimpleImputer(strategy='median')
-	    elif (imputer_s == 'mean'):
-	        imputer = SimpleImputer(strategy='mean')
-	    elif (imputer_s == 'zeros'):
-	        imputer = SimpleImputer(strategy='constant', fill_value=0)
-	    elif (imputer_s == 'function'):
-	        imputer = IterativeImputer()
-	    else:
-	    	raise ValueError('Invalid imputer option provided')
-	    	return 0
-	            
-	    numeric_transformer = Pipeline(steps=[
-	        ('imputer', imputer),
-	        ('scaler', StandardScaler())])
+    # Mutual bit
+    if (imputer_s == 'median'):
+            imputer = SimpleImputer(strategy='median')
+    elif (imputer_s == 'mean'):
+        imputer = SimpleImputer(strategy='mean')
+    elif (imputer_s == 'zeros'):
+        imputer = SimpleImputer(strategy='constant', fill_value=0)
+    elif (imputer_s == 'function'):
+        imputer = IterativeImputer()
+            
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', imputer),
+        ('scaler', StandardScaler())])
 
-	    #categorical_features = ['Embarked', 'Sex', 'Pclass', 'Title']
-	    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+    #categorical_features = ['Embarked', 'Sex', 'Pclass', 'Title']
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
 
-	    preprocessor = ColumnTransformer(
-	        transformers=[
-	            ('num', numeric_transformer, numeric_features),
-	            ('cat', categorical_transformer, categorical_features)])
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, numeric_features),
+            ('cat', categorical_transformer, categorical_features)])
 
 
-	    pipeline = Pipeline(steps=[('preprocessor', preprocessor)])
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor)])
 
-    
+  
 
-    data = pipeline.fit_transform(data)
-    X_train_tr, X_test_tr, y_train, y_test = train_test_split(data, labels, test_size=0.15,
-                                                                random_state=0, stratify=labels)
+
+
+    ########### ATTENZIONE: LE DUE RIGHE DI SEGUITO DEVI METTERLE DOPO IF OPTION == TRAIN TEST!!!!! ##########
+    ########### IN OPTION == GRID SEARCH DEVI INVECE APPLICARE LA PIPELINE A TUTTO IL DATASET (A DATA) 
+    # X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.15,
+    #                                                                random_state=0, stratify=labels)
     # X_train_tr = pipeline.fit_transform(X_train)
     # X_test_tr = pipeline.transform(X_test)
-    print('train_tr shape',X_train_tr.shape)
-    model.fit(X_train_tr, y_train)
-    score_train, score_test = model.score(X_train_tr, y_train), model.score(X_test_tr, y_test)
 
-    return score_train, score_test
+    
+    clf = model
+    
+
+
+    # Baseline
+    if (option == 'train_test'):            
+    
+        data = pipeline.fit_transform(data)
+        X_train_tr, X_test_tr, y_train, y_test = train_test_split(data, labels, test_size=0.20,
+                                                                    random_state=0, stratify=labels)
+        # X_train_tr = pipeline.fit_transform(X_train)
+        # X_test_tr = pipeline.transform(X_test)
+        print('train_tr shape',X_train_tr.shape)
+        clf.fit(X_train_tr, y_train)
+        score_train, score_test = clf.score(X_train_tr, y_train), clf.score(X_test_tr, y_test)
+    
+        return score_train, score_test
+
+
+
+
+
+    # Grid Search
+    elif (option == 'grid_search'):
+        
+        X_train_tr = pipeline.fit_transform(data)  ### NOTA: QUI STAI FITTANDO SU TUTTI I DATI E TRASFORMANDO, MENTRE PRIMA STAVI FITTANDO SUL TRAIN E TRASFORMANDO IL TEST: ASSICURATI CHE NON SIA UN PROBLEMA  
+        y_train = labels
+        print('train_tr shape with gscv', X_train_tr.shape)
+        gscv = GridSearchCV(clf, cv=cv, n_jobs=-3, param_grid=param_grid, scoring='accuracy', verbose=1)
+        gscv.fit(X_train_tr, y_train)
+        best_params = gscv.best_params_
+        best_score = gscv.best_score_
+        best_estimator = gscv.best_estimator_
+        #cv_results = gscv.cv_results_
+
+        return {'best_param': best_params, 'best_score': best_score, 
+        'best_estimator': best_estimator, 'pipeline': pipeline, 
+        'pipeline_params': pipeline.get_params()}
+
+
+    else:
+
+        raise ValueError("No valid option provided, insert 'test_train' or 'grid_search'")
+        return 0
 
 
 
